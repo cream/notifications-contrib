a63492b4c084c733f321ec16cedfcb07bff2b75d
Friedrich Weber <fred@reichbier.de>
2010-06-06 01:47:27 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name = 'Notifications Contrib Libraries',
+    version = '0.1',
+    package_dir = {'cream.contrib.notifications': 'notifications'},
+    packages = ['cream.contrib.notifications'],
+    )
