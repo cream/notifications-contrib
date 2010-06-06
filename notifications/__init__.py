f786602b093b43b8cc6952f0e3999646dad1fe65
Friedrich Weber <fred@reichbier.de>
2010-06-06 02:46:52 +0200

    Added `Frontend` class.

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,6 +1,8 @@
+import gobject
+
 import cream.ipc
 
-from .spec import Server
+from .spec import Server, Notification
 
 FRONTEND_MANAGER_SERVICE_NAME = 'org.cream.notifications.FrontendManager'
 FRONTEND_MANAGER_SERVICE_OBJECT = '/org/cream/notifications/FrontendManager'
@@ -16,8 +18,7 @@ def notification_to_dbus(n):
             n.body,
             n.action_array,
             n.hints,
-            n.expires_timeout)
-
+            n.expire_timeout)
 
 class FrontendManager(cream.ipc.Object):
     __ipc_signals__ = {
@@ -56,4 +57,40 @@ class FrontendManager(cream.ipc.Object):
     @cream.ipc.method('sas', '')
     def register(self, bus_name, capabilities):
         self.frontends[str(bus_name)] = list(capabilities)
+
+class Frontend(cream.ipc.Object):
+    __gsignals__ = {
+        'show-notification': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,)),
+        'hide-notification': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,)),
+    }
+
+    def __init__(self, service_name, service_object, capabilities):
+        cream.ipc.Object.__init__(self, service_name, service_object)
+        self.capabilities = capabilities
+        self.manager = cream.ipc.get_object('org.cream.notifications.FrontendManager')
+        self.notifications = {} # id: Notification
+        # register
+        self.register()
+        # connect to signals
+        self.manager.connect_to_signal('show_notification', self.on_show_notification)
+        self.manager.connect_to_signal('hide_notification', self.on_hide_notification)
+
+    def on_show_notification(self, *args):
+        notification = Notification(*args)
+        self.emit('show-notification', notification)
+        self.notifications[notification.id] = notification
+
+    def on_hide_notification(self, *args):
+        id = args[0]
+        if id in self.notifications:
+            self.emit('hide-notification', self.notifications[id])
+            del self.notifications[id]
+        else:
+            pass # TODO: really just ignore?
+
+    def register(self):
+        """
+            register myself with the frontend manager.
+        """
+        self.manager.register(self._bus_name, self.capabilities)
 
